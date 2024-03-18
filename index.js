0eb5c5bb19fbed1b57aec9b7c23c831c88f7e5d3
AvinashKumar0923 <71658168+AvinashKumar0923@users.noreply.github.com>
2024-03-18 15:25:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-var https = require("follow-redirects").https;
-var fs = require("fs");
+var https = require('follow-redirects').https;
+var fs = require('fs');
 
 var options = {
-  method: "POST",
-  hostname: "customer-analytics-34146.my.salesforce-sites.com",
-  path: "/services/apexrest/buyStocks",
-  headers: {
-    "content-type": "application/json",
-    "bfhl-auth": "2110991887",
-    Cookie:
-      "BrowserId=9iDHkOUDEe6crOVpdDNWXQ; CookieConsentPolicy=0:1; LSKey-c$CookieConsentPolicy=0:1",
+  'method': 'POST',
+  'hostname': 'customer-analytics-34146.my.salesforce-sites.com',
+  'path': '/services/apexrest/createAccount',
+  'headers': {
+    'Content-Type': 'application/json',
+    'Cookie': 'BrowserId=9iDHkOUDEe6crOVpdDNWXQ; CookieConsentPolicy=0:1; LSKey-c$CookieConsentPolicy=0:1'
   },
-  maxRedirects: 20,
+  'maxRedirects': 20
 };
 
 var req = https.request(options, function (res) {
@@ -32,10 +30,10 @@ var req = https.request(options, function (res) {
 });
 
 var postData = JSON.stringify({
-  company: "Bjaj Finserv",
-  currentPrice: 1571.65,
-  accountNumber: "BFHL0018619",
-  githubRepoLink: "https://github.com/AvinashKumar0923/investment-bajaj",
+  "name": "Avinash Kumar",
+  "email": "avinash1887.be21@chitkara.edu.in",
+  "rollNumber": 2110991887,
+  "phone": 6200563822
 });
 
 req.write(postData);
